4712e54ffa7de429895a55ef7f86eb34dd6209dc
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-11-27 06:30:34 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,6 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • GOPAY & DANA 」
-│ •  [+62 812-7367-7810]
+╭─「 Donasi • INDOSAT & XL 」
+│ •  [+62 856-0946-7937]
 │ •  [+62 819-5929-3465]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
