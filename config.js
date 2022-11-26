ede5ffdc58b421177347eba282b58be93ce0325b
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-11-27 06:28:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,11 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['84796102942'],
-  ['62 819-5929-3465'],
-  ['62 812-7367-7810', 'P R A T A M A', true]
+  ['6281959293465'],
+  ['6281273677810'], 
+  ['6281959293465', 'PANGERAN MERMAID', true],
+  ['6281273677810', 'FAIZ JB', true],
+  ['6285609467937', 'BOT', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +42,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '© P R A T A M A  M D'
-  var sticker_author = '© P R A T A M A  M D'
+  var sticker_name = '© FAIZ JB'
+  var sticker_author = '® PANGERAN'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +60,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© P R A T A M A  M D'
+global.wm = '@arrezza20'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
